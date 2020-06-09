4013decde3afd37cbecf081054983a41fa35e918
Rodrigo Barbosa <rodrigo.barba.88@gmail.com>
2020-06-09 20:04:31 -0300

    first commit to repository.

---
+++ src/test/java/br/edu/iesp/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iesp.configserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
